ef75409429f93b67a09a3fbfcbcbb2749358503d
aurelie <lieange1326@outlook.com>
2025-03-13 15:00:19 +0100

    add galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,8 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie","La galerie","/pages/galerie.html"),
     //new route ici
-
+];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
