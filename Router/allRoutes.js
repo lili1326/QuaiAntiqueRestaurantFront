0d27b572a5faccc9d6afd349943c9fd246ca2ea3
aurelie <lieange1326@outlook.com>
2025-03-13 15:45:46 +0100

    add connection

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,9 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie","La galerie","/pages/galerie.html"),
+    new Route("/carte","La  carte","/pages/carte.html"),
+    new Route("/connection","Connection","/pages/connection.html"),
+    new Route("/compte","Creer mon compte","/pages/compte.html"),
     //new route ici
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
