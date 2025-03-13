ce7a7e6a15ff17cb6df8ee36fbcade9bda1f7210
aurelie <lieange1326@outlook.com>
2025-03-13 08:58:31 +0100

    ajout routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,9 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),];
+    //new route ici
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Quai Antique";
