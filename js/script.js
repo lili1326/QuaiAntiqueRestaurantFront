884ace472dc1a52f2f6b4d6f148d2c2dc8bd40fb
aurelie <lieange1326@outlook.com>
2025-04-07 17:29:40 +0200

    add function get info user

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const deconnectionBtn= document.getElementById("deconnection-btn");
 const apiUrl = "http://127.0.0.1:8000/api/";
 
 deconnectionBtn.addEventListener("click",deconnection);
+ 
 
 function getRole(){
     const role = getCookie(RoleCookieName);
@@ -115,3 +116,32 @@ function sanitiezHtml(text){
     tempHtml.textContent = text;
     return tempHtml.innerHTML;
 } 
+
+function getInfosUser(){
+    
+    
+    let myHeaders = new Headers();
+    myHeaders.append("X-AUTH-TOKEN", getToken());
+
+    let requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+    fetch(apiUrl+"account/me", requestOptions)
+    .then(response =>{
+        if(response.ok){
+            return response.json();
+        }
+        else{
+            console.log("Impossible de récupérer les informations utilisateur");
+        }
+    })
+    .then(result => {
+       // console.log(result);
+       return result;
+    })
+    .catch(error =>{
+        console.error("erreur lors de la récupération des données utilisateur", error);
+    });
+}
